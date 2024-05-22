d27eda3df06c4e867a301970b0ba37f070c0c319
Ohidul Islam <ohidul.islam951@gmail.com>
2024-05-23 02:32:42 +0600

    Conditional render header signin buttons

--- templates/header-right-buttons.php
+++ templates/header-right-buttons.php
@@ -1,15 +1,15 @@
 <?php
 
-if ( is_user_logged_in() ) {
+if ( is_user_logged_in() ) :
     echo '<div class="es-logged-in-user">';
         echo '<span class="es-user-name">Jason</span>';
     echo '</div>';
-} else {
-}
-
+else : ?>
+    <div class="es-user-auth-buttons">
+        <button class="es-btn es-btn-signin" id="es-open-signin-modal"><?php echo esc_html__('Sign in'); ?></button>
+        <button class="es-btn es-btn-signup" id="es-open-signup-modal"><?php echo esc_html__('Sign up'); ?></button>
+    </div>
+<?php
+endif;
 ?>
 
-<div class="es-user-auth-buttons">
-    <button class="es-btn es-btn-signin" id="es-open-signin-modal"><?php echo esc_html__('Sign in'); ?></button>
-    <button class="es-btn es-btn-signup" id="es-open-signup-modal"><?php echo esc_html__('Sign up'); ?></button>
-</div>
