58f2b2f492f11aae4276e168e4aad23a8da27006
Ohidul Islam <ohidul.islam951@gmail.com>
2024-07-09 19:32:28 +0600

    Added smooth scrolling to element

--- assets/js/main.js
+++ assets/js/main.js
@@ -463,6 +463,45 @@
             });
         },
 
+        initSmoothScrolling: function() {
+            // Select all links with hashes
+            $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+                // On-page links
+                if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+                && 
+                location.hostname == this.hostname
+                ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                    scrollTop: target.offset().top
+                    }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+                }
+                }
+            });
+        },
+
         init: function() {
             window.engispace.initHeaderFunctions();
             window.engispace.initCourseSliders();
@@ -473,6 +512,7 @@
             window.engispace.initCoursePurchase();
             window.engispace.initSocialShare();
             window.engispace.initStartSelling();
+            window.engispace.initSmoothScrolling();
 
             jQuery(window).on('resize', function() {
                 course_details_hover_box();
