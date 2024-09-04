2a7d7215906a2e9d7c18db7085dc6eb4a2bf3941
Ohidul Islam <ohidul.islam951@gmail.com>
2024-09-04 23:44:26 +0545

    Style updates

---
+++ template-resetpass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Reset Pass
+ */
+
+ get_header();
+?>
+
+<?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post(); 
+            // Get header jumbo
+            get_template_part( 'template-parts/header-jumbo' );
+
+            echo '<div class="es-page-wrapper es-resetpass-wrapper">';
+                echo '<div class="es-site-container">';
+                    the_content();
+                echo '</div>';
+            echo '</div>';
+        } // end while
+    } // end if
+?>
+
+<?php
+get_footer();
+?>
